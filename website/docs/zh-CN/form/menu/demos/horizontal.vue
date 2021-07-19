2da3e9f24f5d99baf93bed8f692d25740a8efd42
huangzhaoping <326598456@qq.com>
2021-07-19 22:38:15 +0800

    update

--- website/docs/zh-CN/form/menu/demos/horizontal.vue
+++ website/docs/zh-CN/form/menu/demos/horizontal.vue
@@ -1,13 +1,17 @@
 <template>
   <div>
     <s-menu active="1" mode="horizontal">
-      <s-menu-item value="1">菜单一</s-menu-item>
-      <s-menu-item value="2">菜单二</s-menu-item>
+      <s-menu-item value="1"><i class="iconfont icon-home" />菜单一</s-menu-item>
+      <s-sub-menu>
+        <template #title>菜单二</template>
+        <s-menu-item value="2-1">菜单2-1</s-menu-item>
+        <s-menu-item value="2-2">菜单2-2</s-menu-item>
+      </s-sub-menu>
       <s-menu-item value="3">菜单三</s-menu-item>
     </s-menu>
     <br/>
     <s-menu active="1" mode="horizontal" theme="dark">
-      <s-menu-item value="1">菜单一</s-menu-item>
+      <s-menu-item value="1"><i class="iconfont icon-home" />菜单一</s-menu-item>
       <s-menu-item value="2">菜单二</s-menu-item>
       <s-menu-item value="3">菜单三</s-menu-item>
     </s-menu>
