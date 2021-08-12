f8c05f58903af6424b71cb0f71b77ee209fdfa62
huangzhaoping <326598456@qq.com>
2021-08-12 23:37:20 +0800

    feat: 完善 tooltip

--- packages/tooltip/main.ts
+++ packages/tooltip/main.ts
@@ -1,12 +1,22 @@
-import { defineComponent, h } from 'vue'
+import { defineComponent, h, Fragment, Ref, ref } from 'vue'
 import Popper from '../popper'
 
 export default defineComponent({
   name: 'STooltip',
+  setup () {
+    const popper: Ref = ref(null)
+    return {
+      popper
+    }
+  },
   render () {
     return h(Popper, { ref: 'popper' }, {
-      default: h(this.$slots.default!),
-      popper: this.$slots.popper!()
+      default: () => h(this.$slots.default!, {
+        onClick: () => {
+          console.log('sadf')
+        }
+      }),
+      popper: () => h(Fragment, null, [h(this.$slots.popper!), h('div', null, 'sadf')])
     })
   }
 })
